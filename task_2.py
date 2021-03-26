cb2c2d9095d8138410f0cc4ef3538bc87dee0c97
rrose666 <uziolko@hotmail.com>
2021-03-26 15:32:56 +0000

    Updated 2nd task

--- task_2.py
+++ task_2.py
@@ -5,19 +5,27 @@
 # - print orders to customer
 
 menu_items = ["1: pizza margarita ", "2: mushroom risotto", "3: pumpkin soup", "4: tomato panini", "5: orange juice", "6: chocolate cake"]
-user_order = input("What would you like to order?")
-completed_order = []
+
 
 def menu():
     for item in menu_items:
         print(f"Menu item {item}")
-
 print("--Restaurant Menu--")
 print(menu())
-print(user_order)
-def order():
-    for i in range(0,user_order):
-        completed_order.append(int(input()))
 
 
+
+completed_order = []
+
+
+print("When you ordered everything you wanted write 'done'")
+order = True
+while order:
+    user_order = input("What else would you like to order:  ")
+    if user_order == "done":
+        order = False
+        break
+    completed_order.append(user_order)
+
+print(completed_order)
 
