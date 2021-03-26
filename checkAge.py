4b02a6ca6e2d6396ca779ae98b5a71bae092f6e4
rrose666 <uziolko@hotmail.com>
2021-03-26 16:56:28 +0000

    Added checkAge file

---
+++ checkAge.py
@@ -0,0 +1,24 @@
+# simple program to use control flow
+
+
+# - You can vote
+# - You can driver
+# - you can't legally drink but your mates/uncles might have your back (bigger 16)
+# - Your too young, go back to school!
+# as a user I should be able to keep being prompted for input until I say 'exit'
+#
+# ## Acceptance Criteria
+#
+# * is a program that run continuously
+# * handles strings and integers
+# * has exist condition
+# * all business logic works
+
+age = 19
+driver_license = True
+
+def check_age():
+    pass
+
+check_age()
+
