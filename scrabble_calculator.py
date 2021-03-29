6048a22623992428c97b2d018ff7874a24ac490c
rrose666 <uziolko@hotmail.com>
2021-03-29 09:50:18 +0100

    Updated

--- scrabble_calculator.py
+++ scrabble_calculator.py
@@ -13,28 +13,37 @@
 # J, X  = 8
 # Q, Z  = 10
 
-def score_calculator():
+class Scrabble_calculator:
 
     def __init__(self):
-        self.score_table = {
-                **dict.fromkeys(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 1),
-                **dict.fromkeys(['D', 'G'], 2),
-                **dict.fromkeys(['B', 'C', 'M', 'P'], 3),
-                **dict.fromkeys(['F', 'H', 'V', 'W', 'Y'], 4),
-                **dict.fromkeys(['K'], 5),
-                **dict.fromkeys(['J', 'X'], 8),
-                **dict.fromkeys(['Q', 'Z'], 10),
-                }
+        self.letter_value = {
+            "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N":1, "R":1, "S": 1, "T": 1,
+            "D": 2, "G": 2,
+            "B": 3, "C": 3, "M": 3, "P": 3,
+            "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+            "K": 5,
+            "J": 8, "X": 8,
+            "Q": 10, "Z": 10
+        }
 
-    def score_word(self, word):
-        if not isinstance(word, str):
-            print('Not a word.')
-            return
-        total = 0
-        for letter in word:
-            value = self.score_table.get(letter.upper())
-            total = total+value if value else total
-        return total
+
+    def calculate_score(self, word):
+        score = 0
+        upper = word.upper()
+
+        for letter in upper:
+            score += self.letter_value[letter]
+
+        return score
+
+word = input("Please enter the scrabble word to check the score : ")
+print("Processing...")
+
+object_scrabble = Scrabble_calculator()
+
+result = object_scrabble.calculate_score(word)
+
+print(f"The score of {word} is: {result} points!")
 
 
 
